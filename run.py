f8a42e477066ac0f7d0e7aa68f7b3136d90095e1
Andrew Damon-Smith <andrewdamonsmith@gmail.com>
2024-09-22 21:13:37 -0400

    Create new run script

---
+++ run.py
@@ -0,0 +1,18 @@
+"""
+This repo is meant to be cloned into the folder of the dump. This is meant to operate on the parent folder.
+"""
+import os
+from src.main import organize_files
+
+# Get the absolute path of the current script
+current_script_path = os.path.abspath(__file__)
+
+# Get the directory containing the script
+current_directory = os.path.dirname(current_script_path)
+
+# Get the parent directory of the current directory
+parent_directory = os.path.dirname(current_directory)
+output_directory = os.path.join(parent_directory, 'photo-flow')
+
+organize_files(source_folder=parent_directory, output_folder=parent_directory)
+
